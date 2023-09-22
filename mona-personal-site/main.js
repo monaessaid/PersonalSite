c375cbdbc6847bfb65fbac64e909cac9842969ef
Mona <2829922e@student.gla.ac.uk>
2023-09-22 22:30:06 +0100

    textures

--- mona-personal-site/main.js
+++ mona-personal-site/main.js
@@ -18,39 +18,43 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-// const geometry2 = new THREE.BoxGeometry(5,5,5);
-// const geometry2 = new THREE.SphereGeometry(4,16,32);
+const geometry = new THREE.TorusGeometry(10, 0.2, 16, 50);
+const geometry3 = new THREE.BoxGeometry(5, 5, 5);
+const geometry2 = new THREE.SphereGeometry(4, 16, 32);
 
-const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
-// const material2 = new THREE.MeshNormalMaterial();
+const material = new THREE.MeshStandardMaterial({ color: "0xFFFF00" });
+const material2 = new THREE.MeshNormalMaterial();
 // const material3 = new THREE.MeshNormalMaterial({wireframe: true});
 // const material3 = new THREE.MeshNormalMaterial({ color: "green", wireframe: true });
 
-const torus = new THREE.Mesh(geometry, material);
+const torus = new THREE.Mesh(geometry, material2);
 // const torus4 = new THREE.Mesh(geometry, material3);
-// const sphere = new THREE.Mesh(geometry2,material3);
+const sphere = new THREE.Mesh(geometry2, material2);
 // const torus2 = new THREE.Mesh(geometry, material2);
 // const torus3 = new THREE.Mesh(geometry, material3);
-// const cube = new THREE.Mesh (geometry2,material);
+// const cube = new THREE.Mesh(geometry3, material);
 
 scene.add(torus);
+scene.add(sphere);
+// scene.add(cube);
 // scene.add(sphere);
 // scene.add(torus4);
 // scene.add(torus2);
 // scene.add(torus3);
 // scene.add(cube);
 
-// const pointLight = new THREE.PointLight(0xff0000, 1, 100);
-// pointLight.position.set(5,5,5);
-// scene.add(pointLight);
+// cube.position.x = 20;
 
-const ambientLight = new THREE.AmbientLight(0xff0000);
+const pointLight = new THREE.PointLight('white', 1, 100);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xe68cff);
 scene.add(ambientLight);
 
-// const sphereSize = 1;
-// const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
-// scene.add (pointLightHelper);
+const sphereSize = 1;
+const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+scene.add(pointLightHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -59,22 +63,86 @@ function addStar() {
     const a = Math.random() * (0.5 - 0.01) + 0.01;
 
     const geometry = new THREE.SphereGeometry(a, 24, 24);
-    const material = new THREE.MeshBasicMaterial( {color: "white"} );
-    const star = new THREE.Mesh(geometry,material);
+    const material = new THREE.MeshNormalMaterial();
+    const star = new THREE.Mesh(geometry, material);
 
-    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
 
-    star.position.set(x,y,z);
+    star.position.set(x, y, z);
     scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
 
+// let letters = "0123456789ABCDEF";
+// let randomColor = '0x';
+
+// for (let i = 0; i < 6; i++) {
+//     randomColor += letters[(Math.floor(Math.random() * 16))];
+// }
+
+// console.log(color);
+
+function addPlanet() {
+    const a = Math.random() * (1 - 0.8) + 0.8;
+    const geometry = new THREE.SphereGeometry(a, 24, 24);
+
+    // let letters = "0123456789abcdef";
+    // let randomColor = '0x';
+
+    // for (let i = 0; i < 6; i++) {
+    //     randomColor += letters[(Math.floor(Math.random() * 16))];
+    // }
+
+    const material = new THREE.MeshNormalMaterial({ flatShading: true });
+    const planet = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+    planet.position.set(x, y, z);
+    scene.add(planet);
+}
+
+Array(50).fill().forEach(addPlanet);
+
+const spaceTexture = new THREE.TextureLoader().load('bg-image4.jpg');
+scene.background = spaceTexture;
+
+const planetTexture1 = new THREE.TextureLoader().load('planet-texture1.jpg');
+const planetTexture2 = new THREE.TextureLoader().load('planet-texture2.jpg');
+
+const texturedPlanet1 = new THREE.Mesh (
+    new THREE.SphereGeometry(15,32,16),
+    new THREE.MeshBasicMaterial( {map: planetTexture1} )
+);
+
+const texturedPlanet2 = new THREE.Mesh (
+    new THREE.SphereGeometry(10,32,16),
+    new THREE.MeshBasicMaterial( {map: planetTexture2} )
+);
+
+scene.add(texturedPlanet1);
+texturedPlanet1.position.x = 25;
+texturedPlanet1.position.y = 25;
+
+scene.add(texturedPlanet2);
+texturedPlanet2.position.x = -25;
+texturedPlanet2.position.y = -25;
+texturedPlanet2.position.z =-25;
+
 function animate() {
     requestAnimationFrame(animate);
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    // torus.rotation.z += 0.01;
+
+    texturedPlanet1.rotation.y += 0.01;
+
+    texturedPlanet2.rotation.y += 0.02;
+
+    // sphere.rotation.x += 0.01;
+    // sphere.rotation.y += 0.005;
+    // sphere.rotation.z += 0.01;
 
     // sphere.rotation.x += 0.05;
     // sphere.rotation.y += 0.05;
