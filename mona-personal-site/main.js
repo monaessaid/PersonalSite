ccd6930d2d1e036854bc71645059b7bff8b8c64a
Mona <2829922e@student.gla.ac.uk>
2023-09-22 16:51:45 +0100

    lighting practice

--- mona-personal-site/main.js
+++ mona-personal-site/main.js
@@ -17,32 +17,53 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const geometry2 = new THREE.BoxGeometry(5,5,5);
+// const geometry2 = new THREE.BoxGeometry(5,5,5);
+// const geometry2 = new THREE.SphereGeometry(4,16,32);
 
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
-const material2 = new THREE.MeshBasicMaterial({ color: "blue", wireframe: true });
-const material3 = new THREE.MeshBasicMaterial({ color: "green", wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+// const material2 = new THREE.MeshNormalMaterial();
+// const material3 = new THREE.MeshNormalMaterial({wireframe: true});
+// const material3 = new THREE.MeshNormalMaterial({ color: "green", wireframe: true });
 
 const torus = new THREE.Mesh(geometry, material);
-const torus2 = new THREE.Mesh(geometry, material2);
-const torus3 = new THREE.Mesh(geometry, material3);
-const cube = new THREE.Mesh (geometry2,material);
+// const torus4 = new THREE.Mesh(geometry, material3);
+// const sphere = new THREE.Mesh(geometry2,material3);
+// const torus2 = new THREE.Mesh(geometry, material2);
+// const torus3 = new THREE.Mesh(geometry, material3);
+// const cube = new THREE.Mesh (geometry2,material);
 
 scene.add(torus);
-scene.add(torus2);
-scene.add(torus3);
-scene.add(cube);
+// scene.add(sphere);
+// scene.add(torus4);
+// scene.add(torus2);
+// scene.add(torus3);
+// scene.add(cube);
+
+// const pointLight = new THREE.PointLight(0xff0000, 1, 100);
+// pointLight.position.set(5,5,5);
+// scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xff0000);
+scene.add(ambientLight);
+
+// const sphereSize = 1;
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+// scene.add (pointLightHelper);
 
 function animate() {
     requestAnimationFrame(animate);
-    torus.rotation.x += 0.1;
-    torus.rotation.y += 0.1;
-    torus.rotation.z += 0.1;
-    torus2.rotation.x += 0.1;
-    torus3.rotation.y += 0.2;
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+    // sphere.rotation.x += 0.05;
+    // sphere.rotation.y += 0.05;
+    // sphere.rotation.z += 0.05;
+    // torus4.rotation.x +=0.05;
+    // torus2.rotation.x += 0.05;
+    // torus3.rotation.y += 0.05;
+    // cube.rotation.x += 0.05;
+    // cube.rotation.y += 0.05;
     renderer.render(scene, camera);
 }
 
-animate()
+animate();
